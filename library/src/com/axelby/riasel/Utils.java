3898ce6cea72980c57876c305acc6ff3ed190fe1
Robert Meijer <robert@grazz.com>
2013-08-10 14:54:15 +0200

    Fixes memory leak, added support for unique id on feed item

--- library/src/com/axelby/riasel/Utils.java
+++ library/src/com/axelby/riasel/Utils.java
@@ -7,44 +7,44 @@ import java.util.Locale;
 import java.util.TimeZone;
 
 public class Utils {
+	
+	final static SimpleDateFormat dateFormats[] = new SimpleDateFormat[] {
+			new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US),
+			new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US),
+			new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.US),
+			new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ", Locale.US),
+			new SimpleDateFormat("EEE, d MMM yy HH:mm:ss z", Locale.US),
+			new SimpleDateFormat("EEE, d MMM yy HH:mm z", Locale.US),
+			new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss z", Locale.US),
+			new SimpleDateFormat("EEE, d MMM yyyy HH:mm z", Locale.US),
+			new SimpleDateFormat("EEE d MMM yy HH:mm:ss z", Locale.US),
+			new SimpleDateFormat("EEE d MMM yy HH:mm z", Locale.US),
+			new SimpleDateFormat("EEE d MMM yyyy HH:mm:ss z", Locale.US),
+			new SimpleDateFormat("EEE d MMM yyyy HH:mm z", Locale.US),
+			new SimpleDateFormat("d MMM yy HH:mm z", Locale.US),
+			new SimpleDateFormat("d MMM yy HH:mm:ss z", Locale.US),
+			new SimpleDateFormat("d MMM yyyy HH:mm z", Locale.US),
+			new SimpleDateFormat("d MMM yyyy HH:mm:ss z", Locale.US),
+
+			new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault()),
+			new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault()),
+			new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.getDefault()),
+			new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ", Locale.getDefault()),
+			new SimpleDateFormat("EEE, d MMM yy HH:mm:ss z", Locale.getDefault()),
+			new SimpleDateFormat("EEE, d MMM yy HH:mm z", Locale.getDefault()),
+			new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss z", Locale.getDefault()),
+			new SimpleDateFormat("EEE, d MMM yyyy HH:mm z", Locale.getDefault()),
+			new SimpleDateFormat("EEE d MMM yy HH:mm:ss z", Locale.getDefault()),
+			new SimpleDateFormat("EEE d MMM yy HH:mm z", Locale.getDefault()),
+			new SimpleDateFormat("EEE d MMM yyyy HH:mm:ss z", Locale.getDefault()),
+			new SimpleDateFormat("EEE d MMM yyyy HH:mm z", Locale.getDefault()),
+			new SimpleDateFormat("d MMM yy HH:mm z", Locale.getDefault()),
+			new SimpleDateFormat("d MMM yy HH:mm:ss z", Locale.getDefault()),
+			new SimpleDateFormat("d MMM yyyy HH:mm z", Locale.getDefault()),
+			new SimpleDateFormat("d MMM yyyy HH:mm:ss z", Locale.getDefault()),
+	};
 
 	static Date parseDate(String date) {
-		final SimpleDateFormat dateFormats[] = new SimpleDateFormat[] {
-				new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US),
-				new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US),
-				new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.US),
-				new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ", Locale.US),
-				new SimpleDateFormat("EEE, d MMM yy HH:mm:ss z", Locale.US),
-				new SimpleDateFormat("EEE, d MMM yy HH:mm z", Locale.US),
-				new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss z", Locale.US),
-				new SimpleDateFormat("EEE, d MMM yyyy HH:mm z", Locale.US),
-				new SimpleDateFormat("EEE d MMM yy HH:mm:ss z", Locale.US),
-				new SimpleDateFormat("EEE d MMM yy HH:mm z", Locale.US),
-				new SimpleDateFormat("EEE d MMM yyyy HH:mm:ss z", Locale.US),
-				new SimpleDateFormat("EEE d MMM yyyy HH:mm z", Locale.US),
-				new SimpleDateFormat("d MMM yy HH:mm z", Locale.US),
-				new SimpleDateFormat("d MMM yy HH:mm:ss z", Locale.US),
-				new SimpleDateFormat("d MMM yyyy HH:mm z", Locale.US),
-				new SimpleDateFormat("d MMM yyyy HH:mm:ss z", Locale.US),
-
-				new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault()),
-				new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault()),
-				new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.getDefault()),
-				new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ", Locale.getDefault()),
-				new SimpleDateFormat("EEE, d MMM yy HH:mm:ss z", Locale.getDefault()),
-				new SimpleDateFormat("EEE, d MMM yy HH:mm z", Locale.getDefault()),
-				new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss z", Locale.getDefault()),
-				new SimpleDateFormat("EEE, d MMM yyyy HH:mm z", Locale.getDefault()),
-				new SimpleDateFormat("EEE d MMM yy HH:mm:ss z", Locale.getDefault()),
-				new SimpleDateFormat("EEE d MMM yy HH:mm z", Locale.getDefault()),
-				new SimpleDateFormat("EEE d MMM yyyy HH:mm:ss z", Locale.getDefault()),
-				new SimpleDateFormat("EEE d MMM yyyy HH:mm z", Locale.getDefault()),
-				new SimpleDateFormat("d MMM yy HH:mm z", Locale.getDefault()),
-				new SimpleDateFormat("d MMM yy HH:mm:ss z", Locale.getDefault()),
-				new SimpleDateFormat("d MMM yyyy HH:mm z", Locale.getDefault()),
-				new SimpleDateFormat("d MMM yyyy HH:mm:ss z", Locale.getDefault()),
-		};
-	
 		for (SimpleDateFormat format : dateFormats) {
 			format.setTimeZone(TimeZone.getTimeZone("UTC"));
 			try {
